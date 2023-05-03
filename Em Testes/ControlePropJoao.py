10af035275d4bbbaff32e1b4b903aedcedcb9e13
Je1pi <joaoecit2019@gmail.com>
2023-05-03 13:11:08 -0300

    Mudanças

--- Em Testes/ControlePropJoao.py
+++ Em Testes/ControlePropJoao.py
@@ -1,41 +1,60 @@
 #!/usr/bin/env pybricks-micropython
 
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor, ColorSensor
+from pybricks.ev3devices import Motor, ColorSensor, UltrasonicSensor
 from pybricks.parameters import Port
 from pybricks.tools import wait
 
-erro = 0
+seguirLinha = True
+desvio = False
+
+erro = valorDesD = valorDesF= 0
 Vb = 150
-Kp = 5
+Kp = 4
 comp = 40       
 
 ev3 = EV3Brick()
 
 motorE = Motor(Port.B)
-motorD = Motor(Port.C)
+motorD = Motor(Port.D)
 
 corE = ColorSensor(Port.S3)
 corD = ColorSensor(Port.S2)
 
+ultraF = UltrasonicSensor(Port.S1)
+ultraD = UltrasonicSensor(Port.S4)
+
 while True:
-    valor_esq = int(corE.reflection())
-    valor_dir = int(corD.reflection())
+    while seguirLinha == True and desvio == False:
 
-    erro =  (valor_esq - valor_dir) + 10
+        valor_esq = int(corE.reflection())
+        valor_dir = int(corD.reflection())
 
-    mE = Vb + Kp*erro
-    mD = Vb - Kp*erro
+        distF = ultraF.distance()
 
-    motorD.run(mD+comp)
-    motorE.run(mE)
+        if distF <= 200:
+            seguirLinha  = False
+            desvio       = True
 
-    # print(valor_esq, valor_dir)
-    #print(erro)
-    #print(mE)
-    #print(mD)
-    #print()
-    #wait(5000)
+        erro = (valor_esq - valor_dir) + 10
 
-    
-    
+        mE = Vb + Kp*erro
+        mD = Vb - Kp*erro
+
+        motorD.run(mD+comp)
+        motorE.run(mE)
+        
+    while seguirLinha == False and desvio == True:
+
+        distF = ultraF.distance()
+        distD = ultraD.distance()
+
+        if distF <= 200:
+            valorDesF = Vb + distF/4
+            valorDesD = Vb - distD/4
+        if distD <= 150:
+            valorDesF = Vb - distF/4
+            valorDesD = Vb + distD/4
+
+        motorE.run(valorDesF)
+        motorD.run(valorDesD)
