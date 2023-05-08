28469e7b3b533323ea7fd4a7a16f55343a9b13e8
Je1pi <joaoecit2019@gmail.com>
2023-05-08 10:05:13 -0300

    Lopa Lopa

--- Em Testes/ControlePropJoao.py
+++ Em Testes/ControlePropJoao.py
@@ -5,14 +5,6 @@ from pybricks.ev3devices import Motor, ColorSensor, UltrasonicSensor
 from pybricks.parameters import Port
 from pybricks.tools import wait
 
-seguirLinha = True
-desvio = False
-
-erro = valorDesD = valorDesF= 0
-Vb = 150
-Kp = 4
-comp = 40
-
 ev3 = EV3Brick()
 
 motorE = Motor(Port.B)
@@ -22,14 +14,14 @@ corE = ColorSensor(Port.S3)
 corD = ColorSensor(Port.S2)
 
 ultraF = UltrasonicSensor(Port.S1)
-ultraD = UltrasonicSensor(Port.S4)
+ultraD = UltrasonicSensor(Port.S4) 
 
-while True:
-
-    motorD.run(0)
-    motorE.run(0)
-
-    while (seguirLinha == True) and (desvio == False):
+def SegueLinhas ():
+    while True:
+        erro = 0
+        Vb = 150
+        Kp = 4
+        comp = 40
 
         valor_esq = int(corE.reflection())
         valor_dir = int(corD.reflection())
@@ -37,8 +29,7 @@ while True:
         distF = ultraF.distance()
 
         if distF <= 200:
-            seguirLinha  = False
-            desvio       = True
+            break
 
         erro = (valor_esq - valor_dir) + 10
 
@@ -47,16 +38,9 @@ while True:
 
         motorD.run(mD+comp)
         motorE.run(mE)
+
+def DesvioObstaculo ():
+    while True:
         
-    motorD.run(0)
-    motorE.run(0)
 
-    while (seguirLinha == False) and (desvio == True):
-
-        distF = ultraF.distance()
-        distD = ultraD.distance()
-
-        print(distD)
-        print(distF)
-
-        wait(1000)
+SegueLinhas()
