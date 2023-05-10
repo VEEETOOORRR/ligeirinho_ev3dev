96609aa1f92a47b862f07b6c01628acbd86d9555
yew-afk <64754484+yew-afk@users.noreply.github.com>
2023-05-10 08:18:57 -0300

    karalho

--- Em Testes/sanguedrogas.py
+++ Em Testes/sanguedrogas.py
@@ -22,5 +22,42 @@ ultraD = UltrasonicSensor(Port.S4)
 cor_dir = ColorSensor(Port.S2)
 cor_esq = ColorSensor(Port.S3)
 
-while True:
-    ev3.speaker.beep(15000,10)
+def ajuste():
+    while True:
+        refD = cor_dir.reflection()
+        refE = cor_esq.reflection()
+        dif = refD - refE
+        if dif < -5:
+            left_motor.run(-10)
+            right_motor.run(40)
+        else:
+            left_motor.run(40)
+            right_motor.run(-10)
+
+def linha():
+    distF = ultraF.distance()
+    while distF > 200:
+        distF = ultraF.distance()
+        refD = cor_dir.reflection()
+        refE = cor_esq.reflection()
+        dif = refD - refE
+
+        velD = vb + kpl*dif
+        velE = vb - kpl*dif
+
+        left_motor.run(velE)
+        right_motor.run(velD)
+
+def desvio():
+    while True:
+        distD = ultraD.distance()
+        distF = ultraF.distance()
+        difD = distD - 200
+
+        velD = vb + kpd*difD
+        velE = vb - kpd*difD
+    
+        left_motor.run(velE)
+        right_motor.run(velD)
+
+linha()
