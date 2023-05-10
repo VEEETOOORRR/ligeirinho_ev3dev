03583340ff640c1e0d6683b96483a49bf2dd5ba6
yew-afk <64754484+yew-afk@users.noreply.github.com>
2023-05-10 08:30:01 -0300

    oscar

--- Em Testes/sanguedrogas.py
+++ Em Testes/sanguedrogas.py
@@ -22,18 +22,6 @@ ultraD = UltrasonicSensor(Port.S4)
 cor_dir = ColorSensor(Port.S2)
 cor_esq = ColorSensor(Port.S3)
 
-def ajuste():
-    while True:
-        refD = cor_dir.reflection()
-        refE = cor_esq.reflection()
-        dif = refD - refE
-        if dif < -5:
-            left_motor.run(-10)
-            right_motor.run(40)
-        else:
-            left_motor.run(40)
-            right_motor.run(-10)
-
 def linha():
     distF = ultraF.distance()
     while distF > 200:
@@ -45,18 +33,6 @@ def linha():
         velD = vb + kpl*dif
         velE = vb - kpl*dif
 
-        left_motor.run(velE)
-        right_motor.run(velD)
-
-def desvio():
-    while True:
-        distD = ultraD.distance()
-        distF = ultraF.distance()
-        difD = distD - 200
-
-        velD = vb + kpd*difD
-        velE = vb - kpd*difD
-    
         left_motor.run(velE)
         right_motor.run(velD)
 
